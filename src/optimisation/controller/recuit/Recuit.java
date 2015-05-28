04b6ff98dc1e925c16861a0f31055cb0a815ea85
Goketsu <benjamin.viraize@gmail.com>
2015-05-28 16:58:20 +0200

    avancement du recuit : clavier entier mais pas opti ?

--- src/optimisation/controller/recuit/Recuit.java
+++ src/optimisation/controller/recuit/Recuit.java
@@ -3,6 +3,7 @@ package optimisation.controller.recuit;
 import java.util.Random;
 
 import optimisation.controller.Algorithme;
+import optimisation.model.Bigramme;
 import optimisation.model.Model;
 import optimisation.model.clavier.Clavier;
 import optimisation.model.clavier.ClavierCollection;
@@ -14,12 +15,12 @@ public class Recuit extends Algorithme{
 	public Recuit(ClavierCollection claviers) {
 		super(claviers);
 		claviers.initCollection(1);
-		temperature = 100000000;
+		temperature = 10000;
 	}
 
 	@Override
 	public void run() {
-		System.out.println("Démarage de l'algorithme recuit");
+		System.out.println("Demarage de l'algorithme recuit");
 		
 		Clavier c = Model.getInstance().getClavierCollection().getClavierCollection().get(0);
 		
@@ -29,11 +30,14 @@ public class Recuit extends Algorithme{
 		char a = '\0';
 		char b = '\0';
 		double distanceMin = 100;
+		double energieMin = 0;
+		double energie = 1;
+		double varEnergie = energie - energieMin;
 		
 		while(temperature > 0){
 			//r = new Random();
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(5);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -50,43 +54,60 @@ public class Recuit extends Algorithme{
 				l = r.nextInt(4);
 				b = c.getKeys()[l][k].getValue();
 			}
-			System.out.println(i);
-			System.out.println(j);
+			//System.out.println(i);
+			//System.out.println(j);
 			double dist = Math.sqrt(Math.pow(i-k, 2) + Math.pow(j-l, 2));
-			if(dist <= distanceMin){
-				if(i < k){
-					if(j < l){
-						c.echange(c.getKeys()[l][k], c.getKeys()[l-1][k-1]);
-						//distanceMin = dist;
-					}else if(j > l){
-						c.echange(c.getKeys()[l][k], c.getKeys()[l+1][k-1]);
-						//distanceMin = dist;						
+			String key = a+""+b;
+			energieMin = 0;
+			energie = dist*Bigramme.getInstance().getValue(key);
+			varEnergie = energieMin - energie; 
+			//System.out.println(key);
+			while(varEnergie != 0 && energie != 0){
+				energie = dist*Bigramme.getInstance().getValue(key);
+				varEnergie = energieMin - energie; 
+				//System.out.println(temperature);
+				//System.out.println("energie : "+energie);
+				//System.out.println(key);
+				System.out.println("proba "+Math.exp((-varEnergie)/temperature));
+				//System.out.println("varEnergie : "+varEnergie);
+				//System.out.println(energie);
+				if(varEnergie < 0 && energie != 0){
+					System.out.println("switch");
+					energieMin = energie;
+					if(i < k){
+						if(j < l){
+							c.echange(c.getKeys()[l][k], c.getKeys()[l-1][k-1]);
+							//energieMin = energie;
+						}else if(j > l){
+							c.echange(c.getKeys()[l][k], c.getKeys()[l+1][k-1]);
+							//energieMin = energie;						
+						}else{
+							c.echange(c.getKeys()[l][k], c.getKeys()[l][k-1]);
+							//energieMin = energie;	
+						}
+					}else if(i >k){
+						if(j < l){
+							c.echange(c.getKeys()[l][k], c.getKeys()[l-1][k+1]);
+							//energieMin = energie;
+						}else if(j > l){
+							c.echange(c.getKeys()[l][k], c.getKeys()[l+1][k+1]);
+							//energieMin = energie;						
+						}else{
+							c.echange(c.getKeys()[l][k], c.getKeys()[l][k+1]);
+							//energieMin = energie;	
+						}
 					}else{
-						c.echange(c.getKeys()[l][k], c.getKeys()[l][k-1]);
-						//distanceMin = dist;	
-					}
-				}else if(i >k){
-					if(j < l){
-						c.echange(c.getKeys()[l][k], c.getKeys()[l-1][k+1]);
-						//distanceMin = dist;
-					}else if(j > l){
-						c.echange(c.getKeys()[l][k], c.getKeys()[l+1][k+1]);
-						//distanceMin = dist;						
-					}else{
-						c.echange(c.getKeys()[l][k], c.getKeys()[l][k+1]);
-						//distanceMin = dist;	
-					}
-				}else{
-					if(j < l){
-						c.echange(c.getKeys()[l][k], c.getKeys()[l-1][k]);
-						//distanceMin = dist;
-					}else if(j > l){
-						c.echange(c.getKeys()[l][k], c.getKeys()[l+1][k]);
-						//distanceMin = dist;						
+						if(j < l){
+							c.echange(c.getKeys()[l][k], c.getKeys()[l-1][k]);
+							//energieMin = energie;
+						}else if(j > l){
+							c.echange(c.getKeys()[l][k], c.getKeys()[l+1][k]);
+							//energieMin = energie;						
+						}
 					}
 				}
 			}
-			System.out.println(dist);
+			//System.out.println("sortie de bouble + baisse de T");
 			temperature--;
 		}
 	}
